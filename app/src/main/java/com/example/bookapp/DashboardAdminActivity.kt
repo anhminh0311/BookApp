d5cde52c067625b936a97aabed0822474bea0f1d
anhminh0311 <163004032+anhminh0311@users.noreply.github.com>
2024-07-02 12:09:46 +0700

    admin dashboard

--- app/src/main/java/com/example/bookapp/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/bookapp/DashboardAdminActivity.kt
@@ -1,20 +1,50 @@
 package com.example.bookapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bookapp.databinding.ActivityDashboardAdminBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class DashboardAdminActivity : AppCompatActivity() {
+class  DashboardAdminActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding: ActivityDashboardAdminBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard_admin)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle click, logout
+        binding.logoutBtn.setOnClickListener{
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+        //get current user
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null){
+            //not logged in, move to main screen
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        else {
+            //logged in, get n show user in4
+            val email = firebaseUser.email
+            //set to text view at toolbar
+            binding.subTitleTv.text = email
         }
     }
 }
--- app/src/main/java/com/example/bookapp/LoginActivity.kt
+++ app/src/main/java/com/example/bookapp/LoginActivity.kt
@@ -131,7 +131,7 @@ class LoginActivity : AppCompatActivity() {
                 }
 
                 override fun onCancelled(error: DatabaseError) {
-                    
+
                 }
             })
     }
