fef091c1eb3c931f646d91001eb1e8ca4741bdbc
anhminh0311 <163004032+anhminh0311@users.noreply.github.com>
2024-07-05 02:32:26 +0700

    show book categories

--- app/src/main/java/com/example/bookapp/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/bookapp/DashboardAdminActivity.kt
@@ -2,12 +2,18 @@ package com.example.bookapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.bookapp.databinding.ActivityDashboardAdminBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class  DashboardAdminActivity : AppCompatActivity() {
 
@@ -17,6 +23,12 @@ class  DashboardAdminActivity : AppCompatActivity() {
     //firebase auth
     private lateinit var firebaseAuth: FirebaseAuth
 
+    //arraylist to hold categories
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+
+    //adapter
+    private lateinit var adapterCategory: AdapterCategory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
@@ -25,6 +37,28 @@ class  DashboardAdminActivity : AppCompatActivity() {
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+        loadCategories()
+
+        //search
+        binding.searchEt.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //called as abd when user type anything
+                try {
+                    adapterCategory.filter.filter(s)
+                }
+                catch (e: Exception){
+
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
 
         //handle click, logout
         binding.logoutBtn.setOnClickListener{
@@ -38,6 +72,35 @@ class  DashboardAdminActivity : AppCompatActivity() {
         }
     }
 
+
+    private fun loadCategories() {
+        //init arraylist
+        categoryArrayList = ArrayList()
+
+        //get all categories from firebase data... Firebase DB > Categories
+        val ref = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //clear list b4 starting adding data into it
+                categoryArrayList.clear()
+                for (ds in snapshot.children){
+                    //get data as model
+                    val model = ds.getValue(ModelCategory::class.java)
+
+                    //add to arraylist
+                    categoryArrayList.add(model!!)
+                }
+                //setup adapter
+                adapterCategory = AdapterCategory(this@DashboardAdminActivity, categoryArrayList)
+                //set adapter to recyclerview
+                binding.categoriesRv.adapter = adapterCategory
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
     private fun checkUser() {
         //get current user
         val firebaseUser = firebaseAuth.currentUser
---
+++ app/src/main/java/com/example/bookapp/FilterCategory.kt
@@ -0,0 +1,58 @@
+package com.techinfosyft.bookappkotln
+
+import android.widget.Filter
+import com.example.bookapp.AdapterCategory
+import com.example.bookapp.ModelCategory
+
+class FilterCategory : Filter {
+
+    //ArrayList in which we want to search
+    private var filterList: ArrayList<ModelCategory>
+
+    //adapter in which filter need to be implemented
+    private var adapterCategory: AdapterCategory
+
+    //constructor
+    constructor(filterList: ArrayList<ModelCategory>, adapterCategory: AdapterCategory) : super() {
+        this.filterList = filterList
+        this.adapterCategory = adapterCategory
+    }
+
+    override fun performFiltering(constraint: CharSequence?): FilterResults {
+        var constraint = constraint
+        val results = FilterResults()
+
+        //value should not be null and not empty
+        if (constraint != null && constraint.isNotEmpty()) {
+            //searched value is nor null not empty
+
+            //change to upper case, or lower case to avoid case sensitivity
+            constraint = constraint.toString().uppercase()
+            val filteredModels: ArrayList<ModelCategory> = ArrayList()
+            for (i in 0 until filterList.size){
+
+                //validate
+                if (filterList[i].category.uppercase().contains(constraint)){
+                    //add to filtered list
+                    filteredModels.add(filterList[i])
+                }
+            }
+            results.count = filteredModels.size
+            results.values = filteredModels
+        }
+        else{
+            //searched value is either null or empty
+            results.count = filterList.size
+            results.values = filterList
+        }
+        return results
+    }
+
+    override fun publishResults(constraint: CharSequence?, results: FilterResults) {
+        //apply filter changes
+        adapterCategory.categoryArrayList = results.values as ArrayList<ModelCategory>
+
+        //notify changes
+        adapterCategory.notifyDataSetChanged()
+    }
+}
